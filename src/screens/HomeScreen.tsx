b481d61e62ee2b06aee41fbef196a8d12e205c09
Leo Rodriguez <leorod18@yahoo.com>
2023-07-15 22:38:17 -0400

    vector icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <MaterialCommunityIcons name="home" color={'red'} size={50} />
     </View>
   )
 }
